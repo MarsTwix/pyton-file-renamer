2cbeb4664e92307aa6d789da1b27c55f05e4fbef
Marnix <ma.wildeman@student.han.nl>
2023-09-14 22:39:23 +0200

    Improve UI layout

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ from tkinter.filedialog import askdirectory, askopenfilenames
 import os
 
 window = tk.Tk()
-
-# window.attributes('-fullscreen', True)
 window.geometry("500x500")
 window.title("Bestand hernoemer")
 
 frame = tk.Frame(master=window)
-textInputFrame = tk.Frame(master=window)
+
+dirLabelFrame = tk.Frame(master=frame)
+selectButtonFrame = tk.Frame(master=frame)
+textInputFrame = tk.Frame(master=frame)
 
 path = ""
 filenames = ()
@@ -22,13 +23,10 @@ def askDir():
         labelDir.config(text=path)
         print(os.listdir(path))
 
-labelDir = tk.Label(frame, text="Geen folder geselecteerd")
-buttonDir = tk.Button(frame, text="Folder selecteren", command=askDir)
+labelDir = tk.Label(dirLabelFrame, text="Geen folder geselecteerd", wraplength=400)
+buttonDir = tk.Button(selectButtonFrame, text="Folder selecteren", command=askDir)
 
-labelDir.grid(row=0, column=0)
-buttonDir.grid(row=0, column=1)
-
-listBox = tk.Listbox(frame)
+listBox = tk.Listbox(window)
 
 def askFiles():
     filenames = askopenfilenames(title='Bestanden selecteren')
@@ -36,11 +34,16 @@ def askFiles():
         for filename in filenames:
             listBox.insert(tk.END, filename)
 
-buttonFiles = tk.Button(frame, text="Bestanden selecteren", command=askFiles)
+buttonFiles = tk.Button(selectButtonFrame, text="Bestanden selecteren", command=askFiles)
 
-buttonFiles.grid(row=1, column=1)
+convertButton = tk.Button(window, text="Bestanden omzetten")
 
-listBox.grid(row=1, column=0)
+labelDir.grid(row=0, column=0)
+dirLabelFrame.grid(row=0)
+
+buttonDir.grid(row=0, column=0)
+buttonFiles.grid(row=0, column=1)
+selectButtonFrame.grid(row=1)
 
 for i in range(len(textInputLabels)):
     label = tk.Label(textInputFrame, text=textInputLabels[i])
@@ -48,9 +51,11 @@ for i in range(len(textInputLabels)):
 
     textInput = tk.Text(textInputFrame, height=1, width=10)
     textInput.grid(row=i, column=1, columnspan=2)
+textInputFrame.grid(row=2)
 
-frame.grid()
+convertButton.grid(row=1, column=0)
+listBox.grid(row=2, column=0)
 
-textInputFrame.grid()
+frame.grid(row=0, sticky="w")
 
 window.mainloop()
