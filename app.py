01bed6d3897db7174bfea26863edbbf3a8617406
Marnix <ma.wildeman@student.han.nl>
2023-09-14 21:10:20 +0200

    Add folder select button

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter.filedialog import askdirectory
+import os
 
 window = tk.Tk()
 
@@ -7,5 +9,22 @@ window.geometry("500x500")
 window.title("File renamer")
 
 frame = tk.Frame(master=window)
+
+path = ""
+
+def askDir():
+    path = askdirectory(title='Folder selecteren')
+    if path:
+        label.config(text=path)
+        print(os.listdir(path))
+        
+
+label = tk.Label(frame, text="Geen folder geselecteerd")
+button = tk.Button(frame, text="Folder selecteren", command=askDir)
+
+label.grid(row=0, column=0)
+button.grid(row=0, column=1)
+
+frame.pack()
 
 window.mainloop()
