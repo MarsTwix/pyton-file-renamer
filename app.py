fa910e8651fc2c8c610054467c7c520496977fb3
Marnix <ma.wildeman@student.han.nl>
2023-09-14 21:03:49 +0200

    Add app.py with tkinter window

---
+++ app.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+window = tk.Tk()
+
+# window.attributes('-fullscreen', True)
+window.geometry("500x500")
+window.title("File renamer")
+
+frame = tk.Frame(master=window)
+
+window.mainloop()
