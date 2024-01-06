5236560f8acdd83a4bae81a383a57c8c7b9c95c1
Marnix <marnix@valkdigital.nl>
2024-01-07 00:21:06 +0100

    Remove interval and begin num and add loader

--- converter.py
+++ converter.py
@@ -1,45 +1,50 @@
 import os
+import gui
 
 path = ""
-filenames = ()
-
-begin_num = "0"
+filenames = []
 prefix = ""
-interval = "1"
 
 MAX_NUMBER_LENGTH = 5
 
-def set_input_values(new_prefix, new_begin_num, new_interval):
+def set_input_values(new_prefix):
     global prefix
-    global begin_num
-    global interval
 
     prefix = new_prefix
-    begin_num = new_begin_num
-    interval = new_interval
 
 def get_filenames(new_path):
     global path
     global filenames
+    global prefix
+
     path = new_path
 
     all_items = os.listdir(path)
 
-    filenames = [item for item in all_items if os.path.isfile(os.path.join(path, item))]
+    filenames = [f for f in all_items if os.path.isfile(os.path.join(path, f))]
+
+    for filename in filenames:
+        if os.path.splitext(filename)[0][-MAX_NUMBER_LENGTH:].isdigit():
+            prefix = os.path.splitext(filename)[0][:-MAX_NUMBER_LENGTH]
+            break
 
     filenames.sort(key=lambda filename: os.stat(os.path.join(path, filename)).st_ctime)
-    
+
     return filenames
 
 def rename_files():
-    global begin_num
+
+    begin_num = 0
+    gui.progressbar.configure(maximum=len(filenames)+0.1)
 
     for filename in filenames:
 
         file_extension = os.path.splitext(filename)[1]
 
-        new_name = prefix + "0" * (MAX_NUMBER_LENGTH - len(begin_num)) + begin_num + file_extension
+        new_name = prefix + "0" * (MAX_NUMBER_LENGTH - len(str(begin_num))) + str(begin_num) + file_extension
 
         os.rename(os.path.join(path, filename), os.path.join(path, new_name))
 
-        begin_num = str(int(begin_num) + int(interval))
+        begin_num = begin_num + 1
+
+        gui.progressbar['value'] = begin_num
